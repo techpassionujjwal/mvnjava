2aaf9e38d9728514acb44a537fe2bae296a13061
Ujjwal Prakash <40907485+techpassionujjwal@users.noreply.github.com>
2021-09-10 09:45:57 +0530

    Update App.java
    unncheck

--- udemo/src/main/java/u/com/example/App.java
+++ udemo/src/main/java/u/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!Ujjwal check" );
+        System.out.println( "Hello World!Ujjwal uncheck" );
     }
 }
